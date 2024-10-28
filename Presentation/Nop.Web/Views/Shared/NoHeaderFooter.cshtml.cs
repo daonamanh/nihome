b22d92cb54885c7b27b42266fddd2e250168c4ea
khoaqd2304 <quachdangkhoa5656@gmail.com>
2024-10-28 16:42:52 +0700

    update 28/10/2024

---
+++ Presentation/Nop.Web/Views/Shared/NoHeaderFooter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nop.Web.Views.Shared
+{
+    public class NoHeaderFooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
